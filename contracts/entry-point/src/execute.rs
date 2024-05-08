c8f8ec24596a17f4c84a7d9ce0b951d615630237
thal0x <91888455+thal0x@users.noreply.github.com>
2024-05-08 18:54:19 -0500

    emit amount out of swap (#110)
    * emit amount out of swap
    
    * make event keys more specific

--- contracts/entry-point/src/execute.rs
+++ contracts/entry-point/src/execute.rs
@@ -524,6 +524,13 @@ pub fn execute_post_swap_action(
         transfer_out_asset
     };
 
+    response = response
+        .add_attribute(
+            "post_swap_action_amount_out",
+            transfer_out_asset.amount().to_string(),
+        )
+        .add_attribute("post_swap_action_denom_out", transfer_out_asset.denom());
+
     match post_swap_action {
         Action::Transfer { to_address } => {
             // Error if the destination address is not a valid address on the current chain
