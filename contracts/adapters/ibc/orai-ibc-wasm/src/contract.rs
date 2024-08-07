b7faaaa0ec59aace21966a9b1c889b9ed630a2d4
trung2891 <trungnv280201@gmail.com>
2024-08-07 10:42:00 +0700

    chore: add migrage entrypoint

--- contracts/adapters/ibc/orai-ibc-wasm/src/contract.rs
+++ contracts/adapters/ibc/orai-ibc-wasm/src/contract.rs
@@ -20,8 +20,28 @@ use skip::{
 ///////////////
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> ContractResult<Response> {
-    unimplemented!()
+pub fn migrate(deps: DepsMut, _env: Env, msg: MigrateMsg) -> ContractResult<Response> {
+    // Validate entry point contract address
+    let checked_entry_point_contract_address =
+        deps.api.addr_validate(&msg.entry_point_contract_address)?;
+    // Validate ibc bridge wasm contract address
+    let checked_ibc_wasm_contract_address =
+        deps.api.addr_validate(&msg.ibc_wasm_contract_address)?;
+
+    // Store the entry point contract address
+    ENTRY_POINT_CONTRACT_ADDRESS.save(deps.storage, &checked_entry_point_contract_address)?;
+    IBC_WASM_CONTRACT_ADDRESS.save(deps.storage, &checked_ibc_wasm_contract_address)?;
+
+    Ok(Response::new()
+        .add_attribute("action", "migrate")
+        .add_attribute(
+            "entry_point_contract_address",
+            checked_entry_point_contract_address.to_string(),
+        )
+        .add_attribute(
+            "ibc_wasm_contract_address",
+            checked_ibc_wasm_contract_address.to_string(),
+        ))
 }
 
 ///////////////////
