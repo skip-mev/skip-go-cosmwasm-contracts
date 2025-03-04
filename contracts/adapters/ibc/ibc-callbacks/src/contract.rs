add638660a6a788d9e5ea8917c71214e320aab37
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2025-03-04 09:48:30 -0700

    run make fmt

--- contracts/adapters/ibc/ibc-callbacks/src/contract.rs
+++ contracts/adapters/ibc/ibc-callbacks/src/contract.rs
@@ -242,7 +242,7 @@ pub fn ibc_destination_callback(
     );
 
     // Require that the packet was successfully received
-    // Need to decode and check string contains bytes at the beginning 
+    // Need to decode and check string contains bytes at the beginning
     // such that StdAck::success(b"\x01").to_binary() doesn't equal
     let ack_str = String::from_utf8_lossy(&msg.ack.data);
     if !ack_str.contains("{\"result\":\"AQ==\"}") {
