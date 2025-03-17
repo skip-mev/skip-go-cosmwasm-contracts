1d5dc685783adae152a2d048dba93846040e072f
99adarsh <itsadarshkumar@gmail.com>
2025-03-17 23:57:53 +0530

    add elys swap adapter contract

---
+++ contracts/adapters/swap/elys/src/error.rs
@@ -0,0 +1,38 @@
+use cosmwasm_std::StdError;
+use skip::error::SkipError;
+use thiserror::Error;
+
+pub type ContractResult<T> = core::result::Result<T, ContractError>;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error(transparent)]
+    Std(#[from] StdError),
+
+    #[error(transparent)]
+    Skip(#[from] SkipError),
+
+    #[error(transparent)]
+    Payment(#[from] cw_utils::PaymentError),
+
+    #[error(transparent)]
+    Overflow(#[from] cosmwasm_std::OverflowError),
+
+    #[error("Unauthorized")]
+    Unauthorized,
+
+    #[error("Parse Int error raised: invalid pool String to pool id u64 conversion")]
+    ParseIntPoolID(#[from] std::num::ParseIntError),
+
+    #[error("swap_operations cannot be empty")]
+    SwapOperationsEmpty,
+
+    #[error("coin_in denom must match the first swap operation's denom in")]
+    CoinInDenomMismatch,
+
+    #[error("coin_out denom must match the last swap operation's denom out")]
+    CoinOutDenomMismatch,
+
+    #[error("Asset Must Be Native, Osmosis Does Not Support CW20 Tokens")]
+    AssetNotNative,
+}
